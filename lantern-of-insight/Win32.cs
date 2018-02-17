b4f4a94f42f359aaf7491eb018d92613cc3efccc
WhereIsWerebear <WhereIsWerebear@protonmail.com>
2018-02-17 14:13:24 -0500

    Add empty Win32 class

---
+++ lantern-of-insight/Win32.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lantern_of_insight
+{
+    class Win32
+    {
+    }
+}
