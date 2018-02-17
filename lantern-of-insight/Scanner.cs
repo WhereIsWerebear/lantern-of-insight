8fd94abb026a959c7beff4e6f846871c936e1615
WhereIsWerebear <WhereIsWerebear@protonmail.com>
2018-02-17 15:39:20 -0500

    Add call to ReadProcessMemory (not working)

--- lantern-of-insight/Scanner.cs
+++ lantern-of-insight/Scanner.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace lantern_of_insight
 {
@@ -12,6 +9,23 @@ namespace lantern_of_insight
         {
             IntPtr hwnd = Win32.FindWindowEx(IntPtr.Zero, IntPtr.Zero, null, "Magic: The Gathering Online");
             Console.WriteLine($"MTGO window handle: {hwnd}");
+
+            const uint FOUR_KILOBYTES = 4 * 1024;
+            byte[] buffer = new byte[FOUR_KILOBYTES];
+            IntPtr numBytesRead = new IntPtr();
+
+            int readProcessMemoryReturn = Win32.ReadProcessMemory(
+                hwnd,
+                IntPtr.Zero,
+                buffer,
+                Convert.ToUInt32(buffer.Length),
+                out numBytesRead);
+
+            if (0 == readProcessMemoryReturn)
+            {
+                string errorMessage = new System.ComponentModel.Win32Exception(Marshal.GetLastWin32Error()).Message;
+                Console.WriteLine($"Error in Win32.ReadProcessMemory(): {errorMessage}");
+            }
         }
     }
 }
