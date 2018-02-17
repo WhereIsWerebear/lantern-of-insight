24287b4b66de4e788c9df79dd4fcb9a553dbe9ff
WhereIsWerebear <WhereIsWerebear@protonmail.com>
2018-02-17 14:23:46 -0500

    Add ReadProcessMemory pinvoke declaration

--- lantern-of-insight/Win32.cs
+++ lantern-of-insight/Win32.cs
@@ -1,12 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Runtime.InteropServices;
 
 namespace lantern_of_insight
 {
     class Win32
     {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,[In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
     }
 }
