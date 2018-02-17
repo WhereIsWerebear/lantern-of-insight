dd7950e2cef5af24f98db6fb6f511ec95bab8600
WhereIsWerebear <WhereIsWerebear@protonmail.com>
2018-02-17 14:47:35 -0500

    Make Win32 members public

--- lantern-of-insight/Win32.cs
+++ lantern-of-insight/Win32.cs
@@ -6,9 +6,9 @@ namespace lantern_of_insight
     class Win32
     {
         [DllImport("kernel32.dll", SetLastError = true)]
-        static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,[In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
+        public static extern Int32 ReadProcessMemory(IntPtr hProcess, IntPtr lpBaseAddress,[In, Out] byte[] buffer, UInt32 size, out IntPtr lpNumberOfBytesRead);
 
         [DllImport("user32.dll", SetLastError = true)]
-        static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
+        public static extern IntPtr FindWindowEx(IntPtr hwndParent, IntPtr hwndChildAfter, string lpszClass, string lpszWindow);
     }
 }
