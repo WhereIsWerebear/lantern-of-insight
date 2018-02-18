178de8e9d9b3a443fd74ba4a8c448163a369d489
WhereIsWerebear <WhereIsWerebear@protonmail.com>
2018-02-17 20:09:52 -0500

    Add pinvoke declarations for OpenProcess and GetWindowThreadProcessId

--- lantern-of-insight/Win32.cs
+++ lantern-of-insight/Win32.cs
@@ -15,6 +15,30 @@ namespace lantern_of_insight
         [DllImport("Oleacc.dll", SetLastError = true)]
         public static extern IntPtr GetProcessHandleFromHwnd(IntPtr hwnd);
 
+        [Flags]
+        public enum ProcessAccessFlags : uint
+        {
+            All = 0x001F0FFF,
+            Terminate = 0x00000001,
+            CreateThread = 0x00000002,
+            VirtualMemoryOperation = 0x00000008,
+            VirtualMemoryRead = 0x00000010,
+            VirtualMemoryWrite = 0x00000020,
+            DuplicateHandle = 0x00000040,
+            CreateProcess = 0x000000080,
+            SetQuota = 0x00000100,
+            SetInformation = 0x00000200,
+            QueryInformation = 0x00000400,
+            QueryLimitedInformation = 0x00001000,
+            Synchronize = 0x00100000
+        }
+
+        [DllImport("kernel32.dll", SetLastError = true)]
+        public static extern IntPtr OpenProcess(ProcessAccessFlags processAccess, bool bInheritHandle, int processId);
+
+        [DllImport("user32.dll", SetLastError=true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
         public enum SE_OBJECT_TYPE
         {
             SE_UNKNOWN_OBJECT_TYPE = 0,
