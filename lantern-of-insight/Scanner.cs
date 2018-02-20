99acfba3e433cda70c7f2f9d666279283dae0043
WhereIsWerebear <WhereIsWerebear@protonmail.com>
2018-02-20 14:53:13 -0500

    Rewrite the main ReadProcessMemory() loop

--- lantern-of-insight/Scanner.cs
+++ lantern-of-insight/Scanner.cs
@@ -18,16 +18,39 @@ namespace lantern_of_insight
             IntPtr hProcess = Win32.OpenProcess(Win32.ProcessAccessFlags.All, false, Convert.ToInt32(processId));
             Console.WriteLine($"MTGO process handle: {hProcess}");
 
-            const uint BUFFER_SIZE = 128;
-            byte[] buffer = new byte[BUFFER_SIZE];
+            // STARTING_ADDRESS and ENDING_ADDRESS were determined by scanning
+            // the Windows 64-bit 8-terabyte address range, [0x000'00000000,
+            // 0x7FF'FFFFFFFF] (' is the 4-byte separator) [1]. STARTING_ADDRESS
+            // and ENDING_ADDRESS make up the only readable range of memory.
+            //
+            // [1] https://docs.microsoft.com/en-us/windows-hardware/drivers/gettingstarted/virtual-address-spaces
+            const uint PAGE_SIZE        = 4 * 1024;
+            const uint STARTING_ADDRESS = 0x240000;
+            const uint ENDING_ADDRESS   = 0x7ffe0000;
+            const uint STARTING_PAGE    = STARTING_ADDRESS / PAGE_SIZE;
+            const uint ENDING_PAGE      = ENDING_ADDRESS / PAGE_SIZE;
+
+
+            byte[] buffer       = new byte[PAGE_SIZE];
             IntPtr numBytesRead = new IntPtr();
 
-            Win32Safe.ReadProcessMemory(
-                hProcess,
-                new IntPtr(0x101038b0),
-                buffer,
-                Convert.ToUInt32(buffer.Length),
-                out numBytesRead);
+            for (uint pageNum = STARTING_PAGE; pageNum <= ENDING_PAGE; ++pageNum)
+            {
+                UInt64 u64BaseAddress = pageNum * PAGE_SIZE;
+
+                Win32Safe.ReadProcessMemory(
+                    hProcess,
+                    new IntPtr(u64BaseAddress),
+                    buffer,
+                    Convert.ToUInt32(buffer.Length),
+                    out numBytesRead);
+
+                if (0 < numBytesRead)
+                {
+                    cout_log << "Read " << numBytesRead << " bytes at address " << hex << u64BaseAddress << dec << endl;
+                }
+            }
+
         }
     }
 }
